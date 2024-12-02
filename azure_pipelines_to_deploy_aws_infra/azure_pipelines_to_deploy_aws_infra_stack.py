bbf9f948ac818b9758abe551dd5b1b5705461f63
sree7k7 <sree7k7@gmail.com>
2024-12-02 13:02:25 +0100

    update code

--- azure_pipelines_to_deploy_aws_infra/azure_pipelines_to_deploy_aws_infra_stack.py
+++ azure_pipelines_to_deploy_aws_infra/azure_pipelines_to_deploy_aws_infra_stack.py
@@ -5,6 +5,9 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+from aws_cdk import aws_s3 as s3
+from aws_cdk import RemovalPolicy
+
 class AzurePipelinesToDeployAwsInfraStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -17,3 +20,7 @@ class AzurePipelinesToDeployAwsInfraStack(Stack):
         #     self, "AzurePipelinesToDeployAwsInfraQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        bucket_s3 = s3.Bucket(self, "AzurePipelinesToDeployAwsInfraBucket",
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY
+        )
